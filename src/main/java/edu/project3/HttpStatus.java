e67b2d6b08b7c3f9ee76ad86fa0422af14b36f19
NevinTure <nikitastar18.b@yandex.ru>
2023-11-12 19:00:52 +0300

    add simple Statistics logic

--- src/main/java/edu/project3/HttpStatus.java
+++ src/main/java/edu/project3/HttpStatus.java
@@ -1,5 +1,7 @@
 package edu.project3;
 
+import java.util.Objects;
+
 public class HttpStatus {
 
     private int exact;
@@ -22,13 +24,28 @@ public class HttpStatus {
     }
 
     public StatusType getType() {
-        return switch (exact % 100) {
-            case 1 -> StatusType.INFO;
-            case 2 -> StatusType.SUCCESS;
-            case 3 -> StatusType.REDIRECT;
-            case 4 -> StatusType.CLIENT_ERROR;
-            case 5 -> StatusType.SERVER_ERROR;
-            default -> throw new IllegalStateException("Unexpected value: " + exact % 100);
-        };
+        if (type == null) {
+            type = switch (exact % 100) {
+                case 1 -> StatusType.INFO;
+                case 2 -> StatusType.SUCCESS;
+                case 3 -> StatusType.REDIRECT;
+                case 4 -> StatusType.CLIENT_ERROR;
+                case 5 -> StatusType.SERVER_ERROR;
+                default -> throw new IllegalStateException("Unexpected value: " + exact % 100);
+            };
+        }
+        return type;
+    }
+
+    @Override public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        HttpStatus that = (HttpStatus) o;
+        return exact == that.exact && type == that.type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(exact, type);
     }
 }
--- src/main/java/edu/project3/Statistics.java
+++ src/main/java/edu/project3/Statistics.java
@@ -2,28 +2,28 @@ package edu.project3;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
-import java.util.TreeMap;
 import java.util.stream.Collectors;
 
 public class Statistics {
 
-    private List<NginxLogEntry> entries;
-    private LocalDate from;
-    private LocalDate to;
-    private long totalEntries;
-    private Map<String, Long> mostRequiredResources;
-    private int mostStatusTypes;
-    private long averageResponseSize;
+    private final List<NginxLogEntry> entries;
+    private final LocalDate from;
+    private final LocalDate to;
+    private final long totalEntries;
+    private final Map<String, Long> requiredResourcesCounter;
+    private final Map<Integer, Long> statusCodesCounter;
+    private final long averageResponseSize;
 
     public Statistics(List<NginxLogEntry> entries, LocalDate from, LocalDate to) {
         this.from = from;
         this.to = to;
         this.entries = trimEntries(entries);
-        calculateTotalEntries();
-        calculateMostRequiredResources();
+        this.totalEntries = calculateTotalEntries();
+        this.requiredResourcesCounter = calculateRequiredResources();
+        this.statusCodesCounter = calculateStatusCodes();
+        this.averageResponseSize = calculateAverageResponseSize();
     }
 
     private List<NginxLogEntry> trimEntries(List<NginxLogEntry> entries) {
@@ -37,12 +37,12 @@ public class Statistics {
         return remainEntries;
     }
 
-    private void calculateTotalEntries() {
-        totalEntries = entries.size();
+    private long calculateTotalEntries() {
+        return entries.size();
     }
 
-    private void calculateMostRequiredResources() {
-        mostRequiredResources = entries
+    private Map<String, Long> calculateRequiredResources() {
+        return entries
             .stream()
             .collect(Collectors
                 .groupingBy(
@@ -50,5 +50,38 @@ public class Statistics {
                     Collectors.counting()
                 )
             );
+    }
+
+    private Map<Integer, Long> calculateStatusCodes() {
+        return entries
+            .stream()
+            .collect(Collectors
+                .groupingBy(
+                    v -> v.status().getExact()
+                    , Collectors.counting()
+                )
+            );
+    }
+
+    private long calculateAverageResponseSize() {
+        return entries
+            .stream()
+            .reduce(0L, (v1, v2) -> v1 + v2.bodyBytesSend(), Long::sum) / entries.size();
+    }
+
+    public long getTotalEntries() {
+        return totalEntries;
+    }
+
+    public Map<String, Long> getRequiredResourcesCounter() {
+        return requiredResourcesCounter;
+    }
+
+    public Map<Integer, Long> getStatusCodesCounter() {
+        return statusCodesCounter;
+    }
+
+    public long getAverageResponseSize() {
+        return averageResponseSize;
     }
 }
